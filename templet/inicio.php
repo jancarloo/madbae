8c60cbc5a6515babe2b4a67eefbee243ab45f94b
JuanCarlosAsus <j.martinezperez@hotmail.com>
2018-07-03 21:22:39 -0500

    codigo a borrar borrar

--- templet/inicio.php
+++ templet/inicio.php
@@ -1,3 +1,11 @@
+<?php 
+$out = $jmy->ver([	
+			"TABLA"=>"blog", 		
+			"COLUMNAS"=>["titulo","subtitulo","imagen","url","fecha"],
+			//"FO"=>true
+			//"ID_F"=>'blog'
+		]);
+$jmyWeb->cargar(["pagina"=>"inicio"]);
 
 $jmyWeb->cargar_js(["url"=>"../app/js/jmy/contacto.js"]); // carga de funciones de cotnacto
 
